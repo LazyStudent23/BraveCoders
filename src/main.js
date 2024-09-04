82d26df987bc7f3fda69fe103b560246a933d957
LazyStudent23 <wizard.alex2016@gmail.com>
2024-09-05 00:03:02 +0300

    create base of project

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/header.js';
+import './js/hero.js';
+import './js/about-me.js';
+import './js/benefits.js';
+import './js/projects.js';
+import './js/faq.js';
+import './js/covers.js';
+import './js/reviews.js';
+import './js/work-together.js';
