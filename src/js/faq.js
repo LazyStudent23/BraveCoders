18b52989c8f5406f2226925182c45774f5203f8f
Supsayan9 <127434945+Supsayan9@users.noreply.github.com>
2024-09-08 14:25:20 +0200

    +

--- src/js/faq.js
+++ src/js/faq.js
@@ -4,13 +4,12 @@ import 'accordion-js/dist/accordion.min.css';
 const container = document.querySelector('.accordion-container');
 
 const accordion = new Accordion('.accordion', {
-    openOnInit: [],  
-    showMultiple: false,  
     duration: 500,  
-    easing: 'ease-in-out',
-    triggerClass: 'btn-svg-opn', 
-    panelClass: 'accordion-content',  
+    showMultiple: false,
+    openOnInit: [],
     elementClass: 'accordion-item', 
+    triggerClass: 'btn-svg-opn',  
+    panelClass: 'accordion-content'
 });
 
 const faqButtons = document.querySelectorAll('.btn-svg-opn');
