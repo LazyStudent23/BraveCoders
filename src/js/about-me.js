b15ea9dd62a13195682d6f39c39ca6de5384e232
LazyStudent23 <wizard.alex2016@gmail.com>
2024-09-09 20:29:53 +0300

    Update about-me.js

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -5,25 +5,25 @@ import { Keyboard, Navigation } from 'swiper/modules';
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 // Обработчик для аккордеона
-document.querySelectorAll('.accordion-container').forEach(container => {
-  container.addEventListener('click', function () {
-    const panel = this.nextElementSibling;
-    // Закрыть все панели кроме текущей
-    document.querySelectorAll('.ac-panel').forEach(p => {
-      if (p !== panel) {
-        p.classList.remove('active');
-        p.previousElementSibling.querySelector('svg').classList.remove('rotate');
-      }
-    });
+// document.querySelectorAll('.accordion-container').forEach(container => {
+//   container.addEventListener('click', function () {
+//     const panel = this.nextElementSibling;
+//     // Закрыть все панели кроме текущей
+//     document.querySelectorAll('.ac-panel').forEach(p => {
+//       if (p !== panel) {
+//         p.classList.remove('active');
+//         p.previousElementSibling.querySelector('svg').classList.remove('rotate');
+//       }
+//     });
 
-    // Открыть/закрыть текущую панель
-    panel.classList.toggle('active');
+//     // Открыть/закрыть текущую панель
+//     panel.classList.toggle('active');
 
-    // Повернуть стрелку
-    const svg = this.querySelector('svg');
-    svg.classList.toggle('rotate');
-  })
-});
+//     // Повернуть стрелку
+//     const svg = this.querySelector('svg');
+//     svg.classList.toggle('rotate');
+//   })
+// });
   const scrollBtn = document.querySelector('.scroll-button');
 
   const acc = new Accordion('.scroll-to-read', {
