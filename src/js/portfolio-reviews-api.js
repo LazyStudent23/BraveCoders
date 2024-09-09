69cbf8dd0c8d5138553d49a015d8b980f020bb9c
Eugene Kuz <tir26@ukr.net>
2024-09-09 09:56:26 +0300

    comment breakpoints

--- src/js/portfolio-reviews-api.js
+++ src/js/portfolio-reviews-api.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
 import axios from 'axios';
 
 export const getReviews = async () => {
   return await axios.get('https://portfolio-js.b.goit.study/api/reviews');
-=======
-export const fetchReviews = async () => {
-  const BaseUrl = 'https://portfolio-js.b.goit.study/api/reviews';
-
-  return await fetch(`${BaseUrl}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
->>>>>>> 606914f (rebase from main 2)
 };
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { getReviews } from './portfolio-reviews-api.js';
 
 import Swiper from 'swiper';
@@ -10,14 +9,6 @@ const reviewCardList = document.querySelector('.review-card-list');
 
 const createReviewTemplate = reviewInfo => {
   return `<li class="swiper-slide review-section-card">
-=======
-import { fetchReviews } from './portfolio-reviews-api.js';
-
-const reviewCardCover = document.querySelector('.review-card-cover');
-
-const createReviewTemplate = reviewInfo => {
-  return `<div class="review-section-card">
->>>>>>> 606914f (rebase from main 2)
       <img
         src="${reviewInfo.avatar_url}"
         class="review-card-avatar"
@@ -27,7 +18,6 @@ const createReviewTemplate = reviewInfo => {
       />
       <p class="reviewer-name">${reviewInfo.author}</p>
       <p class="review-content">${reviewInfo.review}</p>
-<<<<<<< HEAD
     </li>`;
 };
 
@@ -35,19 +25,10 @@ const renderReviewTemplate = async () => {
   try {
     const response = await getReviews();
     const reviewsTemplate = response.data
-=======
-    </div>`;
-};
-
-const renderReviewTemplate = () => {
-  fetchReviews().then(data => {
-    const reviewTemplate = data
->>>>>>> 606914f (rebase from main 2)
       .map(reviewItem => {
         return createReviewTemplate(reviewItem);
       })
       .join('');
-<<<<<<< HEAD
 
     reviewCardList.insertAdjacentHTML('afterbegin', reviewsTemplate);
     swiper.update();
@@ -67,28 +48,20 @@ const swiper = new Swiper('.review-swiper', {
   speed: 400,
   spaceBetween: 200,
   modules: [Navigation, Keyboard],
-  // slidesPerView: 4,
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1440: {
-      slidesPerView: 4,
-    },
-  },
+  slidesPerView: 4,
+  // breakpoints: {
+  //   320: {
+  //     slidesPerView: 1,
+  //   },
+  //   768: {
+  //     slidesPerView: 2,
+  //   },
+  //   1440: {
+  //     slidesPerView: 4,
+  //   },
+  // },
   navigation: {
     prevEl: '.swiper-button-prev',
     nextEl: '.swiper-button-next',
   },
 });
-=======
-    reviewCardCover.insertAdjacentHTML('beforeend', reviewTemplate);
-  });
-  console.log('hello');
-};
-
-renderReviewTemplate();
->>>>>>> 606914f (rebase from main 2)
