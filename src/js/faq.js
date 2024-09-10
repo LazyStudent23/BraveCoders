edddd07f53743261b6f31285ec8cfe272cc4c7cb
Supsayan9 <127434945+Supsayan9@users.noreply.github.com>
2024-09-10 19:45:31 +0200

    Update faq.js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,6 +1,3 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-
 const container = document.querySelector('.accordion-container');
 
 const accordion = new Accordion('.accordion', {
