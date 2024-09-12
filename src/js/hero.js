ecd9762a12a4398b2aacf6cc83a37963546d13e7
LazyStudent23 <wizard.alex2016@gmail.com>
2024-09-12 22:33:33 +0300

    test hero bg load

---
+++ src/js/hero.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const heroSection = document.querySelector('.hero-section');
+  heroSection.style.backgroundImage =
+    "url('../img/hero-section/hero-bg-mobile.png')";
+});
